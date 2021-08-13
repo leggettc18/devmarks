3c034f4e641fb9331648c41dd637a5625b101568
Christopher Leggett <chris@leggett.dev>
2021-08-13 02:18:12 -0400

    Basic rendering of folder info.

--- web/src/components/FolderList.vue
+++ web/src/components/FolderList.vue
@@ -1,5 +1,32 @@
 <template>
     <div>
-        Folder View
+        <div v-for="folder in folders" :key="folder.id">
+            {{folder.name}}
+            <div v-for="bookmark in folder.bookmarks" :key="bookmark.id">
+                <bookmark-component :bookmark="bookmark" />
+            </div>
+        </div>
     </div>
 </template>
+
+<script lang="ts">
+import { useApi } from '@/api/api'
+import { defineComponent, ref } from 'vue'
+import BookmarkComponent from './Bookmark.vue';
+
+export default defineComponent({
+    name: "FolderList",
+    components: {
+        BookmarkComponent
+    },
+    async setup() {
+        const api = useApi();
+        const response = await api.getFolders("bookmarks");
+        const folders = ref(response.data);
+
+        return {
+            folders
+        }
+    },
+})
+</script>
